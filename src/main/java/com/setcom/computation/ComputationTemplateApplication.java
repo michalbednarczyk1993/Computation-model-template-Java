451a546d08079a0a3b12a586484b5799421fb52c
MichalBednarczyk1993 <MichalBednarczyk1993@gmail.com>
2022-03-21 18:54:16 +0100

    Initial commit

---
+++ src/main/java/com/setcom/computation/ComputationTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.setcom.computation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputationTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComputationTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/setcom/computation/ComputationTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.setcom.computation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComputationTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
