3068986732d2f1f683da4ff24c281efd27032a5c
MichalBednarczyk1993 <michal.bednarczyk1993@gmail.com>
2022-04-03 21:26:54 +0200

    status

--- src/main/java/com/setcom/computation/datamodel/PinConfiguration.java
+++ src/main/java/com/setcom/computation/datamodel/PinConfiguration.java
@@ -7,27 +7,24 @@ import java.util.HashMap;
 
 public class PinConfiguration {
 
-    public final String pinName = null;
-    public final String pinType = null;
-    public final String isRequired = null;
-    public final String accessType = null;
-    public final DataMultiplicity dataMultiplicity = null;
-    public final TokenMultiplicity tokenMultiplicity = null;
-    public final HashMap<String, String> accessCredential = null;
-
-    //TODO bez testów nie zrozumiem tego
+    public final String pinName;
+    public final String pinType;
+    public final String isRequired;
+    public final String accessType;
+    public final DataMultiplicity dataMultiplicity;
+    public final TokenMultiplicity tokenMultiplicity;
+    public final HashMap<String, String> accessCredential;
 
     public PinConfiguration(JSONObject section) throws JSONException {
         this.pinName = section.getString("PinName");
         this.pinType = section.getString("PinType");
         this.isRequired = section.getString("IsRequired");
         this.accessType = section.getString("AccessType");
-        this.dataMultiplicity = (DataMultiplicity)Enum.Parse(typeof(DataMultiplicity),
-                section.getString("DataMultiplicity"), true);
-        tokenMultiplicity = (TokenMultiplicity)Enum.Parse(typeof(TokenMultiplicity),
-                section.getString("TokenMultiplicity"), true);
+        this.dataMultiplicity = DataMultiplicity.valueOf(section.getString("DataMultiplicity"));
+        this.tokenMultiplicity = TokenMultiplicity.valueOf(section.getString("TokenMultiplicity"));
         this.accessCredential = new HashMap<>();
-        for (JSONObject aSection : section.getJSONObject("AccessCredential").GetChildren())
-            this.accessCredential.put(aSection.keys().next().toString(), aSection.toString());
+        for (int i = 0; i < section.names().length(); i++) {
+            this.accessCredential.put(section.names().getString(i), section.get(section.names().getString(i)).toString());
+        }
     }
 }
